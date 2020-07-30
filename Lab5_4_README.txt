8383b6fb3ee098ed36c16a75a34c7a5fafbef166
pgj1 <pgj1@users.noreply.github.com>
2020-07-30 07:58:01 -0500

    Add project files.

---
+++ Lab5_4_README.txt
@@ -0,0 +1,31 @@
+﻿LAB: Creating Mario
+Lesson Time: 45 Minutes
+
+In this lab, we will represent the video game character Mario as a struct as a way to start thinking about how 
+complex data is created.
+Scenario:
+You work for a company that allows users to play classic video games on their web site. 
+You’ve been asked to re-create the classic character Mario for use on the website. 
+Using a struct, create various data points that makeup Mario.
+
+1. We need to begin by thinking of all of things that Mario might need. Let’s start making a list.
+Here’s the list we came up with.
+Number of lives
+Number of gold coins
+Current Stage
+Size (small or large)
+Can Shoot a fireball? (yes or no)
+Is jumping right now ( yes or no)
+
+2. Now, let’s turn this into a struct. This code is written ABOVE the Main() Code Block. Do not place it inside of it.
+Don’t worry just yet about the “public” keyword. We’ll discuss that in detail in a future lesson on OOP. 
+We’ve created an enum called Size, integers for lives, a string for the current level, and we are tracking if 
+Mario is jumping and can shoot his fireballs.
+
+
+3. Now in the Main() code block, let’s create Mario and give him some default values.
+Let’s play some Super Mario in C#
+
+Open the 5_4 LabStarter, read through the code. Change the values of Mario as directed as he moves through “the level.”
+
+Starting Code:   https://github.com/ACTWebDev/SD104/blob/master/5_4LabStarter/5_4LabStarter/Program.cs 
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime;
+
+namespace Lab5_4_Mario
+{
+    class Program
+    {
+
+        public struct Mario
+        {
+           public enum Size
+            {
+                small,
+                large
+            }
+           public int numOfLives;
+           public string currentLevel;
+           public Size sizeOfMario;
+           public bool canShootFireBalls;
+           public bool isJumping;
+           public int numOfCoins;
+        }
+
+        static void Main(string[] args)
+        {
+      //    Console.WriteLine("Hello World!");
+            Mario mario = new Mario();
+            mario.numOfLives = 3;
+            mario.currentLevel = "World 1-1";
+            mario.sizeOfMario = Mario.Size.small;
+            mario.canShootFireBalls = false;
+            mario.isJumping = false;
+
+
+            Console.WriteLine("\n Mario is running through the level and meets a Gooba. Set Mario to jumping so he can kill it");
+            //enter your code here.
+            mario.isJumping = true;
+            Console.WriteLine("***** Mario is jumping? " + mario.isJumping);
+
+            Console.WriteLine("\n Mario squashes the Gooba. Now he needs to land!");
+            //enter your code here.
+            mario.isJumping = false;
+            Console.WriteLine("***** Mario is jumping? " + mario.isJumping + ". He landed on the Gooba!");
+
+            Console.WriteLine("\n Mario hits a block and finds a 1UP. Increase Mario's life by one.");
+            //enter your code here.
+            mario.numOfLives += 1;
+            Console.WriteLine("***** Mario has " + mario.numOfLives + " lives now");
+
+            Console.WriteLine("\n Mario finds a secret Fire Flower and can now shoot fireballs. Change mario!");
+            //enter your code here.
+            mario.canShootFireBalls = true;
+            Console.WriteLine("***** Mario can shoot fireballs? " + mario.canShootFireBalls);
+
+
+            Console.WriteLine("\n Mario finds another power up mushroom and gets big. Change Mario");
+            //enter your code here.
+             mario.sizeOfMario = Mario.Size.large; 
+            Console.WriteLine("***** Mario is " + mario.sizeOfMario);
+
+
+            Console.WriteLine("\n Mario finds a stash of gold coins. Give Mario 10 coins.");
+            //enter your code here.
+            mario.numOfCoins += 10;
+            Console.WriteLine("***** Mario has " + mario.numOfCoins + " coins now");
+
+            Console.WriteLine("\n Mario finshes the level! Change Mario's current level to World 1-2");
+            //enter your code here.
+            mario.currentLevel = "World 1-2";
+            Console.WriteLine("***** Congratulations! Mario is entering " + mario.currentLevel + " now!");
+
+        }
+    }
+}
